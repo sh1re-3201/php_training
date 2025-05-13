c83b7674f6f60c64e4849daba38b7aa716af48b0
sh1re-3201 <anugrahmapia@gmail.com>
2025-05-13 13:53:09 +0700

    Initial branch commit

--- regis.php
+++ regis.php
@@ -38,7 +38,7 @@ if (isset($_POST["login"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background-color: #A7C7E7;
+            background-color: #a7c7e8;
         }
 
         .regis-container {
